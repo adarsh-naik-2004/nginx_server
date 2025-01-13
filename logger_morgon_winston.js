d60045e45760a96d3c8511d080fc40b7456acb3c
adarsh-naik-2004 <adarshnaik270@gmail.com>
2025-01-13 12:14:12 +0530

    morgan winston

---
+++ logger_morgon_winston.js
@@ -0,0 +1,27 @@
+/*
+Node Logger is a powerful tool that allows you to log messages to the console, file, or other destinations. It provides a simple and flexible API for logging messages, making it easy to debug and monitor your Node.js applications.
+Console logs are great for quick debugging, but they can be difficult to manage and analyze. In this tutorial, we will explore how to use the advanced Node Logger such as Winston and logger.
+*/
+
+import { createLogger, format, transports } from "winston";
+const { combine, timestamp, json, colorize } = format;
+
+const consoleLogFormat = format.combine(
+  format.colorize(),
+  format.printf(({ level, message, timestamp }) => {
+    return `${level}: ${message}`;
+  })
+);
+
+const logger = createLogger({
+  level: "info",
+  format: combine(colorize(), timestamp(), json()),
+  transports: [
+    new transports.Console({
+      format: consoleLogFormat,
+    }),
+    new transports.File({ filename: "app.log" }),
+  ],
+});
+
+export default logger;
