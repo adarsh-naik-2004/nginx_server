bc850c877cb2f989212112a3c56ade8865d5f38a
adarsh-naik-2004 <adarshnaik270@gmail.com>
2025-02-24 19:32:41 +0530

    redis deploy

--- custom_proxy/src/rate_limiter.ts
+++ custom_proxy/src/rate_limiter.ts
@@ -1,6 +1,6 @@
 import {Redis} from "ioredis";
 
-const redis = new Redis({ host: "127.0.0.1", port: 6379 });
+const redis = new Redis(process.env.REDIS_URL || "redis://127.0.0.1:6379");
 
 export async function rateLimit(ip: string, limit: number, duration: number): Promise<boolean> {
     const key = `rate-limit:${ip}`;
